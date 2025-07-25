7b98c25c0eda9cac1674078d9521a785963b5b05
Pablo Suarez <ps.hernandez27@gmail.com>
2025-07-24 20:24:57 -0500

    Archivo función python
    Función suma

---
+++ funcion.py
@@ -0,0 +1,6 @@
+edad = 36
+telfeno = "123456789"
+print(f"su telefono es: {telfeno}")
+nombre = "Juan"
+apellido = "Perez"
+print(f"su edad es: {edad}, su nombre es: {nombre} y su apellido es: {apellido}")
