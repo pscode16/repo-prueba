c29adc4298ca975ca429feb906032aef43cab243
pscode16 <ps.hernandez27@gmail.com>
2025-07-29 16:07:32 -0500

    Cambio de apellido y telefono

--- funcion.py
+++ funcion.py
@@ -1,6 +1,6 @@
 edad = 36
-telfeno = "123456789"
+telfeno = "3144467686"
 print(f"su telefono es: {telfeno}")
 nombre = "Juan"
-apellido = "Perez"
+apellido = "Suarez"
 print(f"su edad es: {edad}, su nombre es: {nombre} y su apellido es: {apellido}")
